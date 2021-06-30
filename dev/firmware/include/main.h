615ee0efa0e8c6d0fa8683495f85abced7c19192
DBARMPXKOS <dbarmpakos@outlook.com>
2021-06-30 18:21:39 +0300

    Add state machine prototype, add timer for timed sampling

--- dev/firmware/include/main.h
+++ dev/firmware/include/main.h
@@ -1,12 +1,36 @@
+#ifndef FIRMWARE_MAIN_H
+#define FIRMWARE_MAIN_H
+
+/* includes */
 #include "Arduino.h"
 #include <MCP3912.h>
 #include "sauce.h"
 #include "driver/ledc.h"
 #include "../lib/generic_functions/generic_functions.h"
+/* end of includes */
 
-#ifndef FIRMWARE_MAIN_H
-#define FIRMWARE_MAIN_H
+/* PFP */
+void start_clock(uint8_t clkPin, uint32_t frequency);\
+void start_timer(uint8_t timerNum, uint16_t divider, bool countUp, uint64_t interruptAt, bool autoReload, bool enable);
+/* end of PFP */
 
-void start_clock(uint8_t clkPin, uint32_t frequency);
+/* variables */
+typedef enum {
+    sConfig,
+    sSample,
+    sProcess,
+    sTransmit
+} userFSM ;
+
+userFSM state = sConfig;    /* initial state is to configure ADC */
+volatile bool measure;      /* toggled via ISR */
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMUX = portMUX_INITIALIZER_UNLOCKED;
+
+MCP3912 MCP3912(MCP_CS_PIN, MCP_DR_PIN);
+ADCConfig activeConf;       /* this can later be exposed to user settings via GUI */
+
+int32_t samples[1000][4] = {};
+uint16_t sampleCounter = 0;
 
 #endif //FIRMWARE_MAIN_H
--- dev/firmware/src/main.cpp
+++ dev/firmware/src/main.cpp
@@ -1,7 +1,10 @@
 #include "main.h"
 
-MCP3912 MCP3912(MCP_CS_PIN, MCP_DR_PIN);
-ADCConfig activeConf; /* this can later be exposed to user settings */
+void IRAM_ATTR measure_ISR() {
+    portENTER_CRITICAL_ISR(&timerMUX);
+    measure = true;
+    portEXIT_CRITICAL_ISR(&timerMUX);
+}
 
 void setup() {
     Serial.begin(115200);
@@ -10,23 +13,36 @@ void setup() {
     Serial.print(logo);
     Serial.print(firmwareNFO);
 
-    MCP3912.setup_MCP_configuration(activeConf);
-    MCP3912.write_MCP_configuration(activeConf);
-    MCP3912.read_MCP_configuration();
+    if (state == sConfig) {
+        MCP3912.setup_MCP_configuration(activeConf);
+        MCP3912.write_MCP_configuration(activeConf);
+        MCP3912.read_MCP_configuration();
+        state = sSample;
+    }
 
+    start_timer(0, 80, true, 2000000, true, true);
 }
 
 void loop() {
-    bool hasNewData = MCP3912.mcp_data_ready(0x0C);  /* 0x0C STATUSCOM register */
-    while (!hasNewData) { /* waits */ }
-    if (hasNewData) {
-        Serial.printf("\r\nCH0: %i\tCH1: %i\tCH2: %i\tCH3: %i",
-                      MCP3912.read_single_value(0),
-                      MCP3912.read_single_value(1),
-                      MCP3912.read_single_value(2),
-                      MCP3912.read_single_value(3));
+    if (measure && state == sSample) {
+        portENTER_CRITICAL(&timerMUX);
+        for (auto &sample : samples) {
+            sample[0] = MCP3912.read_single_value(0);
+            sample[1] = MCP3912.read_single_value(1);
+            sample[2] = MCP3912.read_single_value(2);
+            sample[3] = MCP3912.read_single_value(3);
+        }
+        measure = false;
+        portEXIT_CRITICAL(&timerMUX);
+        state = sProcess;
     }
 
+    if (state == sProcess) {
+        for (auto &sample : samples) {
+            Serial.println(sample[0]);
+        }
+        state = sTransmit;
+    }
 }
 
 void start_clock(uint8_t clk_pin, uint32_t frequency) {
@@ -46,4 +62,14 @@ void start_clock(uint8_t clk_pin, uint32_t frequency) {
 
     ledc_timer_config(&ledc_timer);
     ledc_channel_config(&ledc_channel);
+}
+
+void start_timer(uint8_t timerNum, uint16_t divider, bool countUp, uint64_t interruptAt, bool autoReload, bool enable) {
+    if (enable) {
+        timer = timerBegin(timerNum, divider, countUp);
+        timerAttachInterrupt(timer, &measure_ISR, true);
+        timerAlarmWrite(timer, interruptAt, autoReload);
+        timerAlarmEnable(timer);
+    } else if (timerAlarmEnabled(timer)) timerAlarmDisable(timer);
+
 }
